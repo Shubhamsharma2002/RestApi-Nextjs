b270ab43b4102da3e1a61b3379ff0917546b7f7d
shubham sharma <shubhamtech004@gmail.com>
2025-04-19 02:06:20 +0530

    db connection , user model schema done

---
+++ libs/Mongodb.ts
@@ -0,0 +1,21 @@
+
+import mongoose from "mongoose";
+// connectic to db function 
+const connectDB = async () => {
+  try {
+    // printing the connection string
+    console.log(`${process.env.DB_URI}`);
+
+    const connection = await mongoose.connect(
+      `${process.env.DB_URI}/${process.env.DB_NAME}`
+    );
+    // connection msg
+    console.log(`databse connect ::)  ${connection}`);
+  } catch (error) {
+    // error msg
+    console.log("mongo db connection error");
+    // console.log(error);
+  }
+};
+
+export default connectDB;
