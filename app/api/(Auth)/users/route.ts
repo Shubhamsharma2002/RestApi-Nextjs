518c6f802bf72dd36627dd47e752fb03d8023855
shubham sharma <shubhamtech004@gmail.com>
2025-04-29 22:32:10 +0530

    user model done

--- app/api/(Auth)/users/route.ts
+++ app/api/(Auth)/users/route.ts
@@ -1,7 +1,15 @@
+import User from "@/libs/Models/User";
+import connectDB from "@/libs/Mongodb"
 import { NextResponse } from "next/server"
 
-export const GET = () =>{
-     return new NextResponse("api setup is working now")
+export const GET = async() =>{
+        try {
+             await connectDB();
+             const users = await User.find();
+             return new NextResponse(JSON.stringify(users),{status:200})
+        } catch (error:any) {
+          return new NextResponse("Error in facting the user" + error.message,{status:500})
+        }
 }
 
 
--- libs/Models/User.ts
+++ libs/Models/User.ts
@@ -1,28 +1,27 @@
-
 import mongoose, { model, models, Schema } from "mongoose";
 
-
 const UserSchema = new Schema(
-     {
-        name:{
-            type:String,
-            required:true
-        },
-        email:{
-            type:String,
-            required:true,
-            unique:true
-        },
-        password:{
-            type:String,
-            required:true
-        }
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-     }
-     ,{
-        timestamps:true
-     }
-)
 
-const User = models.User || model("user", UserSchema);
+const User = models.user || model("user", UserSchema);
+
 export default User;
